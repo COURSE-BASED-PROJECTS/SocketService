8bd0955904b783aa70f45022ecdad3f7c326ca26
baotram11 <19127588@student.hcmus.edu.vn>
2022-08-02 10:55:34 +0700

    test socket with localhost and react

--- src/main/java/com/up/socketservice/controller/ChatController.java
+++ src/main/java/com/up/socketservice/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.up.socketservice.controller;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.up.socketservice.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        logger.info(chatMessage.getContent());
         return chatMessage;
     }
 
